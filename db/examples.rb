56a8fe5739884a9a52ac73d0fa527d2ebaf690c4
Jeffrey Horn <jeffh@generalassemb.ly>
2017-02-07 10:35:58 -0500

    Add books resource and example data

--- db/examples.rb
+++ db/examples.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to experiment with
-# your app during development.
-#
-# The data can then be loaded with the rake db:examples (or created alongside
-# the db with db:nuke_pave).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# %w(antony jeff matt jason).each do |name|
-#   email = "#{name}@#{name}.com"
-#   next if User.exists? email: email
-#   User.create!(email: email,
-#                password: 'abc123',
-#                password_confirmation: nil)
-# end
+require 'csv'
+
+Book.transaction do
+  CSV.foreach(Rails.root + 'data/books.csv',
+              headers: true,
+              header_converters: -> (h) { h.downcase.to_sym }) do |book_row|
+    book = book_row.to_hash
+    Book.create book unless Book.exists? book
+  end
+end
