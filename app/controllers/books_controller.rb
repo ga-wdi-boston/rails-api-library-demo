335eef0cca3eda17a677649612b1c2e4bcb10ac5
payne-chris-r <06cpayne@gmail.com>
2017-04-18 20:08:10 -0400

    Add full feature for create for books
    Successfully adds a full feature for a POST to /books which routes
    to the BooksController's create action which updates the db and returns
    the newly created book rendered as json. Adds book_params to filter
    out unwanted requests or poorly formatted data.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,20 @@ class BooksController < ApplicationController
   def show
     render json: Book.find(params[:id])
   end
+
+  def create
+    @book = Book.create(book_params)
+
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+
+  private :book_params
 end
