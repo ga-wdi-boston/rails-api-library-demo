5a762af253354dc03cd15a9650f93f2549e4105f
payne-chris-r <06cpayne@gmail.com>
2017-04-20 11:44:24 -0400

    Adds author reference to books
    Books now contain author_id which is a foreign key, referencing the
    id of the Author who the book was written by (belongs_to).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419183303) do
+ActiveRecord::Schema.define(version: 20170420154013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170419183303) do
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 20170419183303) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "books", "authors"
   add_foreign_key "examples", "users"
 end
