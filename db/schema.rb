c30f9ee964bdedfbeea0bf0c6a8f7dded1ebfdb5
Samantha Guergenenov <samguergen@gmail.com>
2017-05-02 09:24:54 -0400

    end of sam's course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428140302) do
+ActiveRecord::Schema.define(version: 20170428182147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170428140302) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
-    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -47,5 +48,6 @@ ActiveRecord::Schema.define(version: 20170428140302) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "books", "authors"
   add_foreign_key "examples", "users"
 end
