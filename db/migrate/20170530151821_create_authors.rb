3b23fbb58e7c1e9b37eccfabbd3dcbaa6bbdd8de
Ben Adamski <benjadamski@gmail.com>
2017-05-30 11:53:37 -0400

    Migration File
    - Edit create_authors  migration

--- db/migrate/20170530151821_create_authors.rb
+++ db/migrate/20170530151821_create_authors.rb
@@ -1,10 +1,11 @@
 class CreateAuthors < ActiveRecord::Migration[5.0]
   def change
     create_table :authors do |t|
-      t.string :given_name
-      t.string :family_name
+      t.string :given_name, null: false
+      t.string :family_name, null: false
 
       t.timestamps
     end
   end
 end
+   
--- scripts/authors/create-author.sh
+++ scripts/authors/create-author.sh
@@ -2,7 +2,7 @@ curl --include --request POST http://localhost:4741/authors \
   --header "Content-Type: application/json" \
   --data '{
     "author": {
-      "family_name": "Example Title",
-      "given_name": "Example Author"
+      "family_name": "Langloid",
+      "given_name": "Tizzoni"
     }
   }'
