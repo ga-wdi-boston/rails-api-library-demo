1228aaf5864c96c36d076395b583edcf39160b7a
Ben Adamski <benjadamski@gmail.com>
2017-05-30 11:27:50 -0400

    Migrate changes
    - from initial demo commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418173948) do
+ActiveRecord::Schema.define(version: 20170530151821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "given_name"
+    t.string   "family_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
