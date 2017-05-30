753189d7c298e9011bed9d849fadc26c93672672
Ben Adamski <benjadamski@gmail.com>
2017-05-30 11:19:08 -0400

    Demo: Scaffold Author Routes, Controller, Model, and Serializer
    - Ran generate command.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  resources :authors
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
