ab00813ba764bbdf8dc632e13f9461ca15151b08
payne-chris-r <06cpayne@gmail.com>
2017-07-13 10:19:10 -0400

    Moves data from books -> authors table
    Pulls author data out of books and puts into and Author table. Since we
    already had a ton of data about Authors in our database. In order to
    make use of the data we already had, we created a migration that takes
    data out of the Books table and inserts it into the Authors table while
    associating that book with the newly created Author.

---
+++ db/migrate/20170713005716_move_author_data_to_authors.rb
@@ -0,0 +1,45 @@
+class MoveAuthorDataToAuthors < ActiveRecord::Migration[5.0]
+  def authors
+    Book.all.pluck(:author).map do |author|
+      author.split(' ')
+    end
+  end
+
+  def author_to_names(author)
+    names = {}
+    names[:given_name] = if author.length == 3
+                           "#{author[0]} #{author[1]}"
+                         else
+                           author.first
+                         end
+    names[:surname] = author.last
+    names
+  end
+
+  def create_authors
+    authors.each do |author|
+      author_params = author_to_names(author)
+
+      next if Author.find_by(author_params)
+      Author.create!(author_params)
+    end
+  end
+
+  def associate_authors_with_books
+    Author.all.each do |author|
+      name = "#{author.given_name} #{author.surname}"
+      Book.where(author: name).each do |book|
+        book.update!(author: author)
+      end
+    end
+  end
+
+  def up
+    create_authors
+    associate_authors_with_books
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
